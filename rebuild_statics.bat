cf8746667e38b8428876ed30af63512de159559e
Colin Basnett <cmbasnett@gmail.com>
2024-06-06 18:20:58 -0700

    Fixed command line args

--- rebuild_statics.bat
+++ rebuild_statics.bat
@@ -1,2 +1,3 @@
 cd /D "%~dp0"
-.\venv\Scripts\activate && python .\bdk.py --mod DarkestHourDev build --name_filter=*.usx
+.\venv\Scripts\activate && python .\bdk.py --mod DarkestHourDev build --name_filter=*.usx --clean
+pause
